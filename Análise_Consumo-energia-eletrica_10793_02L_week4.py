294dbc6be8469bf242692954b98a878af57e392f
pfragoso77 <134558690+pfragoso77@users.noreply.github.com>
2023-06-25 19:18:03 +0100

    Arrumar o código e colocar comentários

--- Análise_Consumo-energia-eletrica_10793_02L_week4.py
+++ Análise_Consumo-energia-eletrica_10793_02L_week4.py
@@ -6,11 +6,12 @@ Created on Sun Jun 25 17:12:13 2023
 
 # Importação da biblioteca Pandas para a realização de uma análise de dados
 import pandas as pd
+
 # tarefa 1 > importar dados da tabela Excel 
 dados_analise = pd.read_excel('P_Data_Extract_From_World_Development_Indicators.xlsx', sheet_name='Data')
 #print(dados_analise)
 
-# tarefa 2 > limpeza de dados
+# tarefa 2 > limpeza de dados, somente os que interessam para o efeito
 dados_analise = dados_analise[['Country Name', 'Country Code', '1990 [YR1990]', '2000 [YR2000]']]
 #print(dados_analise)
 
@@ -18,16 +19,17 @@ dados_analise = dados_analise[['Country Name', 'Country Code', '1990 [YR1990]', 
 dados_portugal = dados_analise[dados_analise['Country Name'] == 'Portugal']
 print(dados_portugal)
 
-# Valores encontrados no dataframe
+# Valores específicos no dataframe que interessam para a análise
 consumo_inicial = dados_portugal['1990 [YR1990]'].values[0]
 consumo_final = dados_portugal['2000 [YR2000]'].values[0]
 
+#definição para calcular a variaçãp
 def calcular_variacao_percentual(consumo_inicial, consumo_final):
     return (consumo_final - consumo_inicial) / consumo_inicial * 100
 
 
-# Função para calcular a variação percentual
+# Chamar a função de cálcuo e imprimir o resultado
 variacao_percentual = calcular_variacao_percentual(consumo_inicial, consumo_final)
 
-# Imprimir o resultado
+# Imprimir o resultado (esta foi dura...)
 print(f"A variação percentual no consumo de energia elétrica em Portugal entre 1990 e 2000: {variacao_percentual:.2f}%.")
